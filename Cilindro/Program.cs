f4f05cfed1690991a64ba7ef954e881d847d1dd1
Maria Margarida <mmargarida688rgmail.com>
2025-02-27 22:53:38 +0000

    Added Cilindro project to LP1Semana02 solution.

---
+++ Cilindro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Altura ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Raio: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+            double area_superficie = 2 * Math.PI * raio * (raio + altura);
+
+            Console.WriteLine($"Volume: {volume}");
+            Console.WriteLine($"Área da superfície: {area_superficie}");
+        }
+    }
+}
+
