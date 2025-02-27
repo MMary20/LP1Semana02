e394cf8ec8c46db2c5a89fef0a7c375dbdb90e7b
Maria Margarida <mmargarida688rgmail.com>
2025-02-27 22:40:37 +0000

    Added VariousOperators project to LP1Semana02 solution.

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um número inteiro não negativo: ");
+            byte numero = Convert.ToByte(Console.ReadLine()); 
+
+            Console.WriteLine(numero / 2);            
+            Console.WriteLine(numero << 3);          
+            Console.WriteLine(numero ^ 6);             
+            Console.WriteLine(numero > 10);           
+        }
+    }
+}
+
