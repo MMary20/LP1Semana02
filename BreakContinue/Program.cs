f6c73d8e934dc0efad1cf3faf609a5f3ebe18af0
Maria Margarida <mmargarida688rgmail.com>
2025-02-27 22:23:45 +0000

    Adding BreakContinue project to LP1Semana02 solution and modified with the exercise.

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BreakContinue
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int maxTurns = 20;
+            int turn = 0;
+            while (true)
+            {
+                turn++;
+
+                if (turn >= maxTurns)
+                {
+                    break;
+                }
+                if (turn % 3 == 0)
+                {
+                    continue;
+                }
+                if (turn == 10)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"Turn number {turn}");
+            }
+
+            Console.WriteLine($"Last turn was {turn}");
+        }
+    }
+}
+
+
