0e8b86531b685f01a65b3c129ad6ba11d3ba0467
Maria Margarida <mmargarida688rgmail.com>
2025-02-27 21:46:09 +0000

    Added SmoothOperator project in LP1Semana02 solution.

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira número inteiro: ");
+        
+        if (int.TryParse(Console.ReadLine(), out int numero))
+        {
+            sbyte n = (sbyte)numero;
+            
+            Console.WriteLine(n - 1);  
+            Console.WriteLine(n + 1);  
+        }
+    }
+}
